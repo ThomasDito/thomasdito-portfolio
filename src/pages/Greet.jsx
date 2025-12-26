9654c8de9f0bf699efa25edaadf721cfa6c05fd3
thomas <thmsdito@gmail.com>
2025-12-26 16:46:06 +0700

    update v1.2: adjust loading img

--- src/pages/Greet.jsx
+++ src/pages/Greet.jsx
@@ -11,7 +11,7 @@ const Greet = () => {
     return(
         <div className='container mx-auto h-screen w-screen flex flex-col gap-6 justify-center items-center p-4'>
             <div className='aspect-video w-full sm:w-[500px] overflow-hidden mx-auto'>
-                <img src={image.banner} alt="404 banner" loading='lazy' className='h-full w-full object-cover rounded-4xl' />
+                <img src={image.banner} alt="404 banner" loading='eager' className='h-full w-full object-cover rounded-4xl' />
             </div>
             <h1 className='text-center text-2xl font-bold'>Thank you for your response...</h1>
             <p className='max-w-prose mx-auto text-center text-base/loose opacity-60'>
